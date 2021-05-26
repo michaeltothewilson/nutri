c1717e7b01fc13c05d53bdc49f1353762c7a4dfa
shawnspears <shspears@pdx.edu>
2021-05-26 13:57:23 -0700

    Add grid to dashboard page, add first card

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,30 @@
+import './Home.css';
+
 export default function Home() {
   return (
     <>
       <h2 className="page-title pt-5">Dashboard</h2>
+      <div className="container">
+        <section class="grid-container mt-4">
+          <div class="grid-item item1">
+            <div class="card">
+              <div class="card-header">
+                Nutrients
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="grid-item item2">
+
+          </div>
+          <div class="grid-item item3">
+
+          </div>
+        </section>
+      </div>
     </>
   );
 }
