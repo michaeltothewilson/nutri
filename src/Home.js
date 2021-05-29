c7ce34ca24777cdb63427a53cf2fd2e4cbdadd32
shawnspears <shspears@pdx.edu>
2021-05-29 11:58:25 -0700

    Add side cards to the right of the dashboard and style

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import './Home.css';
+import { Link } from "react-router-dom";
+import breakfast from './assets/images/undraw_breakfast_psiw.svg';
+import hamburger from './assets/images/undraw_Hamburger_8ge6.svg';
 
 export default function Home() {
   return (
     <>
       <h2 className="page-title pt-5">Dashboard</h2>
       <div className="container">
-        <section className="grid-container mt-4">
+        <section className="dashboard-grid-container mt-4">
           <div className="grid-item item1">
             <div className="card">
               <div className="card-header">
@@ -46,10 +49,27 @@ export default function Home() {
             </div>
           </div>
           <div className="grid-item item2">
-
+            <div className="card">
+              <div className="card-body">
+                <img className="img-fluid" src={breakfast} alt="A graphic of an English Breakfast plate in between a bottle of ketchup and a glass of iced soda" />
+                <div className="mt-3 text-center">
+                  <Link to="/browse">
+                    <button type="button" class="btn btn-outline-success">Add to your food log</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="grid-item item3">
-
+            <div className="card">
+              <div className="card-body">
+                <img className="img-fluid" src={hamburger} alt="A graphic of a man next to an unusually large hamburger" />
+                <div className="mt-3">
+                  <h4>Your food log is empty!</h4>
+                  <p>Add food to your log to see your dashboard fill up</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
