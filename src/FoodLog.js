76a168e7521a97503b02d197031b52f6ff3442f8
Abbigail Utley <utleyab@pdx.edu>
2021-05-29 07:47:13 -0700

    gotta make it importable

--- src/FoodLog.js
+++ src/FoodLog.js
@@ -1,10 +1,17 @@
-//storage.json is local storage of foods
-
+//storage.json is temporary local storage of foods
+//daily intake that I'm going off of: 
+//fiber: 30g
+//protein: 50g
+//carbs: 275g
+//water: 3 liters
+//sugar: 30g
 import {useState} from "react"
+const Emoji = require('./getEmojiCount.js');
 
 export default function FoodLog() {
 
   const [data, setData]=useState(null)
+
   fetch("./storage.json").then(
     function(res){
       return res.json()
@@ -19,20 +26,57 @@ export default function FoodLog() {
   return (
     <>
       <h2 className="page-title pt-5">Food Log</h2>
-      <div className="foodlog">
-        {
-          data? data.map(
-            function(data) {
-              return (
-                <ul className="foodlog">
-                  <li className="title">What you ate today:</li>
-                  <li className="items">{data[0].name}</li>
-                  <li className="items">{data[1].name}</li>
-                </ul>
-              )
-            }
-          ):""
-        }
+      <div className="grid-container">
+        <ul className="foodlog">
+          <li className="title">What you ate today:</li>  
+          {
+            data? data.map(
+              function(data) {
+                let emojiCart = []
+                let emojiString = ""
+                let emoji = ""
+                let fiberNum = Emoji.getEmojiCount("fiber", data.fiber)
+                let proteinNum = Emoji.getEmojiCount("protein", data.protein)
+                let carbsNum = Emoji.getEmojiCount("carbs", data.carbs)
+                let waterNum = Emoji.getEmojiCount("water", data.water)
+                let sugarNum = Emoji.getEmojiCount("sugar", data.sugar)
+                emojiCart.push(fiberNum)
+                emojiCart.push(proteinNum)
+                emojiCart.push(carbsNum)
+                emojiCart.push(waterNum)
+                emojiCart.push(sugarNum)
+                for(let i = 0; i < emojiCart.length; ++i) {
+                  switch(i) {
+                    case 0:
+                      emoji = '<Emoji symbol="💩" />'
+                      break
+                    case 1:
+                      emoji = "&#128170;"
+                      break
+                    case 2: 
+                      emoji = "&#129366;"
+                      break
+                    case 3:
+                      emoji = "&#128167;"
+                      break
+                    case 4:
+                      emoji = "&#127853;"
+                      break
+                    default:
+                      emoji = "&#127774;"
+                      break
+                  }
+                  for(let j = 0; j < emojiCart[i]; ++j) {
+                    emojiString += emoji
+                  } 
+                }
+                return (
+                  <li className="item">{data.name} emojis: {emojiString}</li>
+                )
+              }
+            ):""
+          }
+        </ul>
       </div>
     </>
   );
--- src/getEmojiCount.js
+++ src/getEmojiCount.js
@@ -41,3 +41,4 @@ function getEmojiCount(typeOfFood, amt) {
     }
     return num;
 }
+module.exports = { getEmojiCount }
